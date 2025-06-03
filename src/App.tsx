34a784f9cbccf334a48300aa2167c0aa3173e847
ntm-phuong <ntmphuonglao@gmail.com>
2025-06-03 16:10:18 +0700

    create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <p>Flower shop of Lamb </p>
+      
+    </>
+  )
+}
+
+export default App
